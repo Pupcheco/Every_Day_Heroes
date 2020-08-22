8485c40a2059e8a031871c9bf7fae02bf8eb2bee
Austin Tasato <aytasato@gmail.com>
2020-08-21 19:12:37 -0700

    added prototype destructable mock scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject subject;
+    Vector3 positionOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionOffset = this.transform.position - subject.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = subject.transform.position + positionOffset; 
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform camera;
+    public Transform target; 
+
+    public float movementSpeed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        this.rb.AddForce(movement * movementSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
