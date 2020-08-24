1d1913ffe3cd8b26b3e82f0902bb640778235bbd
Austin Tasato <aytasato@gmail.com>
2020-08-23 20:39:51 -0700

    added UI with reactive logic in scripts
    - Added NotoMono as the primary font.
    - The UI is setup such that you only need to set
      the public variables on the scripts for it to
      update correctly.
    - Installed UIExtensions to be able to use a few
      of their prefabs.

---
+++ Assets/Scripts/UI/HPLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPLogic : MonoBehaviour
+{
+    public UnityEngine.UI.Text numberText;
+    public UnityEngine.UI.Slider slider;
+
+    public int currentHp;
+    public int maxHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numberText.text = currentHp.ToString();
+        slider.value = currentHp / ((float) maxHp);
+    }
+}
---
+++ Assets/Scripts/UI/TimeLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLogic : MonoBehaviour
+{
+    public UnityEngine.UI.Text numberShadowText;
+    public UnityEngine.UI.Text numberText;
+
+    public int secondsLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var minutes = secondsLeft / 60;
+        var seconds = secondsLeft % 60;
+        var text = minutes.ToString()  + ":" + seconds.ToString();
+        numberShadowText.text = text;    
+        numberText.text = text;    
+    }
+}
