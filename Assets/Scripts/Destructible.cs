13d157f8f04a22e4eb08940714015c859595b670
Rastal <rastal@protonmail.com>
2020-08-24 11:30:22 -0700

    Update the destructibles script to work with collisions

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+    [SerializeField] GameObject _shatteredPrefab;
+    [SerializeField] float _necessaryImpactSize = 10f;
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.impulse.magnitude > _necessaryImpactSize && collision.gameObject.layer == 11) {
+            Object.Instantiate(_shatteredPrefab, this.transform.position, this.transform.rotation);
+            Object.Destroy(this.gameObject);
+        }
+    }
+}
