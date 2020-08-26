f09f178e7da172b663ea0d1f07dcb73bf7036fb7
Rastal <rastal@protonmail.com>
2020-08-25 23:13:24 -0700

    Fix the Bolt graph for firing off explosions

---
+++ Assets/Scripts/Demo Scripts/ExplosionTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class ExplosionTest : MonoBehaviour {
+    
+    [SerializeField] GameObject _explosionObject;
+    bool _exploded = false;
+
+    void Update() {
+        if (Time.time > 3f && !_exploded) {
+            CustomEvent.Trigger(_explosionObject, "Set Visible");
+            _exploded = true;
+        }
+    }
+}
