8265409037f4ebd0d2887c421d434b8687e9ac5b
Rastal <rastal@protonmail.com>
2020-08-30 14:13:46 -0700

    Set up the rest of the explosion effects

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -3,20 +3,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bolt;
 
 public class Destructible : MonoBehaviour {
     [SerializeField] GameObject _shatteredPrefab;
     [SerializeField] float _necessaryImpactSize = 10f;
     [FMODUnity.EventRef, SerializeField] string _destructionSound = "";
+    DustClouds _dustCloudsObject;
 
     void OnCollisionEnter(Collision collision) {
         if (collision.impulse.magnitude > _necessaryImpactSize && collision.gameObject.layer == 11) {
-            Object.Instantiate(_shatteredPrefab, this.transform.position, this.transform.rotation);
-            Object.Destroy(this.gameObject);
+            var shatteredObject = Object.Instantiate(_shatteredPrefab, this.transform.position, this.transform.rotation);
+            shatteredObject.layer = LayerMask.NameToLayer("Debris");
+            foreach (var shard in shatteredObject.GetComponentsInChildren<Rigidbody>()) {
+                shard.AddForce(collision.impulse, ForceMode.Impulse);
+            }
+
+            var dustCloudPosition = new Vector3(this.transform.position.x, 0f, this.transform.position.z);
+            _dustCloudsObject.TriggerDustCloud(dustCloudPosition);
 
             if (_destructionSound != "") {
                 FMODUnity.RuntimeManager.PlayOneShot(_destructionSound, this.transform.position);
             }
+            
+            Object.Destroy(this.gameObject);
         }
+    }
+
+    void OnEnable() {
+        this.gameObject.layer = LayerMask.NameToLayer("Destructibles");
+        _dustCloudsObject = GameObject.Find("Dust Clouds").GetComponent<DustClouds>();
     }
 }
---
+++ Assets/Scripts/DustClouds.cs
@@ -0,0 +1,30 @@
+﻿#pragma warning disable 649
+#pragma warning disable 414
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class DustClouds : MonoBehaviour { 
+    
+    const int _dustCloudPoolCount = 10;
+    [SerializeField] GameObject _dustCloudPrefab;
+    [FMODUnity.EventRef, SerializeField] string _destructionSound = ""; 
+    Queue<GameObject> _dustClouds = new Queue<GameObject>();
+
+    public void TriggerDustCloud(Vector3 position) {
+        var cloud = _dustClouds.Dequeue();
+        cloud.transform.position = position;
+        CustomEvent.Trigger(cloud, "Building collapse");
+        _dustClouds.Enqueue(cloud);
+    }
+
+    void Start() {
+        for (var i = 0; i < _dustCloudPoolCount; ++i) {
+            var cloud = GameObject.Instantiate(_dustCloudPrefab, this.transform.position, this.transform.rotation, this.transform);
+            _dustClouds.Enqueue(cloud);
+        }
+    }
+
+}
